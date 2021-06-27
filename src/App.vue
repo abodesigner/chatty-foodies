a3cd83390c9417897338066b67c4dca7cd8715e8
abodesigner <mohammed.abosalim@gmail.com>
2021-06-27 16:45:46 +0200

    simple change

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,10 @@
 
 <style lang="scss">
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   font-family: "Karla", sans-serif;
-  /* font-family: 'Varela Round', sans-serif; */
-  //background-color: #fbfbfb;
 }
 
 #nav {
